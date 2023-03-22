8d7286100e328106c411d414267444a9f5338044
CHOCOCHANEL <jh780465@naver.com>
2023-03-22 22:04:30 +0900

    Chapter2. class and method - 20230322

---
+++ Chapter02/src/MyProfileImpl.java
@@ -0,0 +1,6 @@
+public class MyProfileImpl {
+    public static void main(String[] args) {
+        MyProfile profile = new MyProfile("choco-chanel", 25);
+        System.out.println(profile);
+    }
+}
